fb238a43b5e4e52f364a40c73b6e8e9e65473270
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-04-22 16:03:41 -0700

    jobs are listed by company name

--- src/components/Companies.js
+++ src/components/Companies.js
@@ -2,15 +2,19 @@ import React from "react";
 
 function Companies(companyData){
     // console.log((companyData))
+    // is there a cleaner/better way to write this?
+    // both work?
+    // const companies = Object.entries(companyData)[0][1]
+    const companies = Object.values(companyData)[0]
+    const companiesToDisplay = companies.map((company)=>
+        <div key={company.id}>
+            <h1 className="companyName">{company.company_name}</h1>
+            {company.jobs.map((job)=> <p key={job.id}>{job.position}</p>)}
+        </div>
+    )
 
-    const data = Object.entries(companyData)[0][1]
-    // console.log(data)
-    
-    data.map((company)=>{
-        console.log(company.company_name)
-    })
     return(<div>
-        {data.map((company)=> <h1 key={company.id}>{company.company_name}</h1>)}
+        {companiesToDisplay}
 
     </div>)
 }
