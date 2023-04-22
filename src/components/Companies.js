00709d9877d4b57eaf4c3f5d80a3b24d6c9c7fc8
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-04-22 13:46:08 -0700

    managed to iterate over my API

--- src/components/Companies.js
+++ src/components/Companies.js
@@ -1,8 +1,16 @@
 import React from "react";
 
 function Companies(companyData){
-    console.log(companyData)
+    // console.log((companyData))
+
+    const data = Object.entries(companyData)[0][1]
+    // console.log(data)
+    
+    data.map((company)=>{
+        console.log(company.company_name)
+    })
     return(<div>
+        {data.map((company)=> <h1 key={company.id}>{company.company_name}</h1>)}
 
     </div>)
 }
