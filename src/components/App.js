602ab0e23d5699108f61df700b336bc0e618dc2a
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-04-22 12:57:06 -0700

    added fetch request to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,15 @@
 import '../styles/App.scss';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [companyData, setCompanyData] = useState([])
+  useEffect(()=>{
+    fetch("http://localhost:9292/companies")
+    .then((res)=>res.json())
+    .then(companyInfo=>setCompanyData(companyInfo))
+  },[])
+
+  console.log(companyData)
   return (
     <div className="App">
   
