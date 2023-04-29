5d24434fcceaa6c2b85d7fe8231f57431bc9a4d5
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-04-29 15:06:23 -0700

    building routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,25 @@
 import '../styles/App.scss';
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
+import { BrowserRouter, Routes,Route, Link } from 'react-router-dom';
 
-import Companies from './Companies';
+// import Companies from './Companies';
+import Home from './Home';
 
 function App() {
-  const [companyData, setCompanyData] = useState([])
-  useEffect(()=>{
-    fetch("http://localhost:9292/companies")
-    .then((res)=>res.json())
-    .then(companyInfo=>setCompanyData(companyInfo))
-  },[])
+  // const [companyData, setCompanyData] = useState([])
+  // useEffect(()=>{
+  //   fetch("http://localhost:9292/companies")
+  //   .then((res)=>res.json())
+  //   .then(companyInfo=>setCompanyData(companyInfo))
+  // },[])
 
   return (
     <div className="App">
-      <Companies companyData={companyData}/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Companies.js
+++ src/components/Companies.js
@@ -1,24 +1,28 @@
 import React from "react";
 import "../styles/Companies.scss"
 
-function Companies(companyData){
-    // console.log((companyData))
+function Companies({companyData}){
+    console.log((companyData))
     // is there a cleaner/better way to write this?
     // both work?
     // const companies = Object.entries(companyData)[0][1]
-    const companies = Object.values(companyData)[0]
-    const companiesToDisplay = companies.map((company)=>
-    // console.log(company.logo_url)
+    // const companies = Object.values(companyData)[0]
+    // console.log(companyData)
+    const companiesToDisplay = companyData.map((company)=>
+    // console.log(company.jobs)
+    // <>
         <div className="company-card">
-                {/* display companies here */}
                 <img src={company.logo_url} alt="company logo" className="logo-image"/>
-
         </div>
+    //     {/* <h1>{company.jobs}</h1> */}
+    // </>
     )
-
-    return(<div className="company-grid">
+    // debugger
+    return(<div className="content">
+        <div className="company-grid">
         {companiesToDisplay}
 
+        </div>
     </div>)
 }
 
