06b9e1895a24f8dd8f937616392c768d2e4fcb5c
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-05-03 21:42:29 -0700

    added styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,17 @@ import "../styles/Home.scss"
 function Home({companyData}){
     // const jobs = companyData.map((company)=>company.jobs.map((job)=>job.position))
     // console.log(companyData.map((company)=>company.jobs.map((job)=>job)))
-    const jobsToDisplay = companyData.map((company)=>(<div className="job-card">
-        {company.jobs.map((job)=><>
+    const jobsToDisplay = companyData.map((company)=>(
+        company.jobs.map((job)=>
+        <div className="job-card" key={job.id}>
+            <div className="content">
             <h4 className="job-description">{job.position}</h4>
             <p className="company-name">{company.company_name}</p>
             <p className="location">{job.location}</p>
             <p className="job-description">{job.job_description}</p>
-        </>)}
-    </div>))
+            </div>
+        </div>)
+    ))
 
     return(
         <div className="home">
