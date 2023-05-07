ec1294f7ad8cfed8fff6875cec763395afd9753b
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-05-07 10:46:53 -0700

    added the company logo to CompanyContent

--- src/components/CompanyContent.js
+++ src/components/CompanyContent.js
@@ -2,6 +2,10 @@ import "../styles/Home.scss"
 
 function CompanyContent({companyData}){
 
+    /*
+    is there a way to "combine" company content and home? Home takes in a collection of objects while CompanyContent takes a single object
+    */
+
     const jobsToDisplay = companyData.jobs.map((job)=>
         <div className="job-card" key={job.id}>
             <div className="content">
@@ -14,6 +18,7 @@ function CompanyContent({companyData}){
 
     return(
         <div className="company-content">
+            <img src={companyData.logo_url} className="company-logo" alt="Company logo" />
             <div className="job-content">
             {jobsToDisplay}
             </div>
