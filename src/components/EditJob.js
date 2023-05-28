23e07fee98459074015a0f7ec77a9294b7d303bd
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-05-28 08:14:55 -0700

    building EditJobForm

--- src/components/EditJob.js
+++ src/components/EditJob.js
@@ -3,9 +3,10 @@ import { useState } from "react"
 
 
 function EditJob({job}){
+
     const [jobBody, setJobBody] = useState(job)
-    console.log(jobBody)
-        function handleJobChange(e){
+
+    function handleJobChange(e){
         let name = e.target.name
         let value = e.target.value
         setJobBody({...job, [name]:value})
@@ -13,26 +14,29 @@ function EditJob({job}){
 
     function handleEditJobSubmit(e){
         e.preventDefault()
-        console.log(job)
+        console.log(jobBody)
     }
 
-    // function handleDeleteJobClick(e){
-    //     e.preventDefault()
-    //     console.log(job.id)
-    //     // fetch(`http://localhost:9292/jobs/${job.id}`,{
-    //     //     method: "DELETE"
-    //     // })
-    //     // .then(()=> handleDeleteJob(job.id))
-    //     // console.log(job.id)
+    function handleDeleteJobClick(e){
+        e.preventDefault()
+        console.log(job.id)
+        // fetch(`http://localhost:9292/jobs/${job.id}`,{
+        //     method: "DELETE"
+        // })
+        // .then(()=> handleDeleteJob(job.id))
+        // console.log(job.id)
 
-    // }
+    }
     return(
-        <form>
-            <input className="input" type="text" name="position" value={job.description} placeholder="" onChange={handleJobChange} />
+        <form className="edit-job" onSubmit={handleEditJobSubmit}>
+            <input className="input" type="text" name="position" value={jobBody.description} placeholder="" onChange={handleJobChange} />
             {/* <input className="input" type="" name="" value={} placeholder="" onChange={handleJobChange} /> */}
-            <input className="input" type="text" name="location" value={job.location} placeholder="" onChange={handleJobChange} />
-            <input className="input" type="integer" name="pay" value={job.pay} placeholder="" onChange={handleJobChange} />
-            <input className="input" type="text" name="job_description" value={job.job_description} placeholder="" onChange={handleJobChange} />
+            <input className="input" type="text" name="location" value={jobBody.location} placeholder="" onChange={handleJobChange} />
+            <input className="input" type="number" name="pay" value={jobBody.pay} placeholder="" onChange={handleJobChange} />
+            <input className="input" type="text" name="job_description" value={jobBody.job_description} placeholder="" onChange={handleJobChange} />
+            <div className="edit-btns">
+            <button type="submit">edit job</button> <button type="click" onClick={handleDeleteJobClick}>delete</button>
+            </div>
         </form>
     )
 }
