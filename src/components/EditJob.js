1c33fe9f64087f1328673fb743b341e96f9aba14
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-05-30 12:51:43 -0700

    building update handleUpdate function

--- src/components/EditJob.js
+++ src/components/EditJob.js
@@ -3,24 +3,26 @@ import { useState, useContext } from "react"
 import { CompanyContext } from "./Context/companies"
 
 
-function EditJob({job}){
+function EditJob({job, setEditIsOn, editIsOn}){
     const {companyData, setCompanyData} = useContext(CompanyContext)
-    // console.log(companyData.map((company)=>company.company_name))
 
-    const [jobBody, setJobBody] = useState(job)
+    const [jobBody, setJobBody] = useState({
+        position: job.position,
+        job_description: job.job_description,
+        pay: job.pay,
+        location: job.location,
+        company_id: job.company_id
+    })
+
+    const {position, job_description, pay, location, company_id} = jobBody
 
     function handleJobChange(e){
         let name = e.target.name
         let value = e.target.value
-        setJobBody({...job, [name]:value})
-    }
-    console.log(job)
-
-    function handleEditJobSubmit(e){
-        e.preventDefault()
+        setJobBody({...jobBody, [name]:value})
         // console.log(jobBody)
     }
-
+    
     function deleteJob(deleteJob){
         const companyDataToUpdate = [...companyData]
         const company = companyDataToUpdate.find((company)=>company.id === deleteJob.company_id)
@@ -33,24 +35,49 @@ function EditJob({job}){
         fetch(`http://localhost:9292/jobs/${job.id}`,{
             method: "DELETE"
         })
-        // .then((r) => r.json())
         .then(()=> deleteJob(job))
-        deleteJob(job)
-        // console.log(job.id)
+        
+    }
 
+    function handleUpdate(updatedJob){
+        console.log(updatedJob)
+        if (job.company_id === updatedJob.company_id){
+            const companyDataToUpdate = [...companyData]
+            const company = companyDataToUpdate.find((company)=>company.id === job.id)
+            const i = company.jobs.findIndex((job)=>job.id === updatedJob.id)
+            company.jobs[i] = updatedJob
+            // console.log(companyDataToUpdate)
+            setCompanyData(companyDataToUpdate)
+            // debugger
+        } else{
+            deleteJob(job)}
+    }
+
+    function handleEditJobSubmit(e){
+        e.preventDefault()
+        fetch(`http://localhost:9292/jobs/${job.id}`,{
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(jobBody)
+        })
+        .then((res)=>res.json())
+        .then((updatedJob)=>handleUpdate(updatedJob))
+        setEditIsOn(!editIsOn)
     }
 
     const companySelectOptions = companyData.map((company)=>[company.company_name,company.id])
     // debugger
     return(
         <form className="edit-job" onSubmit={handleEditJobSubmit}>
-            <input className="input" type="text" name="position" value={jobBody.position} placeholder="" onChange={handleJobChange} />
-            <select type="number" name="company_id" value={jobBody.company_id} onChange={handleJobChange}>
+            <input className="input" type="text" name="position" value={position} placeholder="" onChange={handleJobChange} />
+            <select type="number" name="company_id" value={company_id} onChange={handleJobChange}>
                 {companySelectOptions.map((company)=><option name="company_id" value={company[1]} key={company[1]}>{company[0]}</option>)}
             </select>
-            <input className="input" type="text" name="location" value={jobBody.location} placeholder="" onChange={handleJobChange} />
-            <input className="input" type="number" name="pay" value={jobBody.pay} placeholder="" onChange={handleJobChange} />
-            <input className="input" type="text" name="job_description" value={jobBody.job_description} placeholder="" onChange={handleJobChange} />
+            <input className="input" type="text" name="location" value={location} placeholder="" onChange={handleJobChange} />
+            <input className="input" type="number" name="pay" value={pay} placeholder="" onChange={handleJobChange} />
+            <input className="input" type="text" name="job_description" value={job_description} placeholder="" onChange={handleJobChange} />
             <div className="edit-btns">
             <button type="submit">edit job</button> <button type="click" onClick={handleDeleteJobClick}>delete</button>
             </div>
--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -6,7 +6,7 @@ function JobCard({job, company}){
     const [editIsOn, setEditIsOn] = useState(false)
     return(
         <div className="job-card" onDoubleClick={() => setEditIsOn((editIsOn)=>!editIsOn)}>
-            {editIsOn? <EditJob job={job} /> :(<div className="content">
+            {editIsOn? <EditJob job={job} setEditIsOn={setEditIsOn} editIsOn={editIsOn} /> :(<div className="content">
             <h4 className="job-position">{job.position}</h4>
             <p className="company-name">{company}</p>
             <p className="location">{job.location}</p>
